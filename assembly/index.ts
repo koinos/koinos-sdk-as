f1766827976e2302cde38ed4c1b9229ab7659445
roaminroe <roamin@tuta.io>
2022-04-14 15:38:29 -0400

    export koinos-proto-as

--- assembly/index.ts
+++ assembly/index.ts
@@ -7,3 +7,22 @@ export { Token } from './token';
 export * from './util';
 export { MockVM } from './mockVM';
 export { SafeMath } from './safeMath';
+
+export * from './koinos-proto-as/google/protobuf/any';
+
+export * from './koinos-proto-as/koinos/chain/authority';
+export * from './koinos-proto-as/koinos/chain/chain';
+export * from './koinos-proto-as/koinos/chain/events';
+export * from './koinos-proto-as/koinos/chain/object_spaces';
+export * from './koinos-proto-as/koinos/chain/system_call_ids';
+export * from './koinos-proto-as/koinos/chain/system_calls';
+export * from './koinos-proto-as/koinos/chain/value';
+
+export * from './koinos-proto-as/koinos/protocol/protocol';
+
+export * from './koinos-proto-as/koinos/contracts/pow/pow';
+export * from './koinos-proto-as/koinos/contracts/resources/resources';
+export * from './koinos-proto-as/koinos/contracts/token/token';
+
+export * from './koinos-proto-as/koinos/common';
+export * from './koinos-proto-as/koinos/options';
